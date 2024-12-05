565cd633c82f4cecf926d6c354b851dade310249
KrishnaWanusha <krishnawanusha@gmail.com>
2024-12-05 09:06:45 +0530

    component: button

--- components/button.component.tsx
+++ components/button.component.tsx
@@ -1,28 +1,39 @@
 type ButtonComponentProps = {
   title: string;
+  type?: "primary" | "secondary";
   className?: string;
   spinner?: boolean;
   disabled?: boolean;
   onClick?: () => void;
+  icon?: JSX.Element;
 };
 
 export default function ButtonComponent({
   title,
+  type = "primary",
   className,
   spinner,
   disabled,
   onClick,
+  icon,
 }: ButtonComponentProps) {
   return (
     <>
       <button
         onClick={onClick}
         disabled={disabled || spinner}
-        className={`rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm 
-            hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
-            focus-visible:outline-indigo-600 ${className ?? ""} `}
+        className={`rounded-md px-3.5 py-1.5 text-sm font-semibold shadow-sm 
+      ${
+        type === "primary"
+          ? "bg-gradient-to-r from-indigo-500 to-indigo-700 text-white hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          : "bg-white text-black border-2 border-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+      }
+      ${className ?? ""}`}
       >
-        {title}
+        <div className="flex items-center justify-center space-x-2">
+          {icon && <span className="inline-block w-4">{icon}</span>}
+          <span>{title}</span>
+        </div>
       </button>
     </>
   );
